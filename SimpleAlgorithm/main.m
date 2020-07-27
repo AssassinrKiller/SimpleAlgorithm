3ec999b8e71424d5c46ac0755afec5aa350f8eb8
AssassinrKiller <ios_service@126.com>
2020-07-27 22:33:51 +0800

    寻找最长回文字符串

--- SimpleAlgorithm/main.m
+++ SimpleAlgorithm/main.m
@@ -12,7 +12,7 @@
 
 
 
-//冒泡   3,2,5,4,1,6
+//冒泡排序   3,2,5,4,1,6
 void BubbleSort1(int *arr, int aLen){
     for (int i = 0; i < aLen; i++) {
         for (int j = 0; j < (aLen - i - 1); j++) {
@@ -22,17 +22,10 @@ void BubbleSort1(int *arr, int aLen){
                 arr[j+1] = temp;
             }
         }
-//        for (int p = 0; p < aLen; p++) {
-//            printf("%d",arr[p]);
-//        }
-//        printf("\n");
     }
 }
-//插入排序  3,2,5,4,1,6
-/**
- 2,3,5,4,1,6
- */
 
+//插入排序  3,2,5,4,1,6
 void InsertSort(int *arr, int aLen){
     int j,temp;
 //    for (int i = 1; i < aLen; i++) {
@@ -51,10 +44,9 @@ void InsertSort(int *arr, int aLen){
         }
         arr[j+1] = temp;
     }
-    
 }
 
-//选择排序 3,2,5,4,1,6
+//选择排序
 void SelectedSort(int *a, int aLen){
     int index;
     for (int i = 0; i < aLen; i++) {
@@ -72,8 +64,6 @@ void SelectedSort(int *a, int aLen){
     }
 }
 
-
-
 //打印杨辉三角形状并使用*填充
 void testPrint(int n) {
     for (int i = 0; i < n; i++) {
@@ -82,6 +72,45 @@ void testPrint(int n) {
         }
         printf("\n");
     }
+}
+
+/**
+ 寻找最长回文字符串  @[@"a",@"b",@"v",@"c",@"c",@"v"]
+ 下面这种是暴力解法,比较笨
+ */
+void LongestPalindromicSubstring(NSArray *str){
+    int sLen = (int)str.count;
+    int jump = 0,
+         max = 0,
+      begain = 0,
+         end = 0;
+    
+    for (int i = 0; i < sLen; i++) {
+        for (int j = i+1; j < sLen; j++) {
+            if (str[i] == str[j]) {
+                int p = i,q = j;
+                while (str[p] != str[j] && p < q) {
+                    jump = 1;
+                    break;
+                }
+                p++;
+                q--;
+                if (jump == 1) {
+                     break;
+                }
+                if (max < j - i) {
+                    max = j - i;
+                    end = j;
+                    begain = i;
+                }
+            }
+        }
+    }
+    NSString *result = @"";
+    for (int i = begain; i <= end; i++) {
+        result = [NSString stringWithFormat:@"%@%@",result,str[i]];
+    }
+    NSLog(@"%@",result);
 }
 
 int main(int argc, const char * argv[]) {
@@ -94,7 +123,7 @@ int main(int argc, const char * argv[]) {
 //        NSArray *result = [XHMergeSortedList mergeListWithListA:@[@1,@3,@5] ListB:@[@2,@4,@6,@8]];
 //        NSLog(@"%@",result);
         
-        int arr[] = {3,2,5,4,1,6};
+//        int arr[] = {1,2,5,3,7,6,4};
 //        BubbleSort1(arr,6);
 //        for (int i = 0; i < 6; i++) {
 //            printf("%d \n",arr[i]);
@@ -107,18 +136,15 @@ int main(int argc, const char * argv[]) {
 //            printf("%d",arr[p]);
 //        }
         
-        SelectedSort(arr,6);
-        for (int p = 0; p < 6; p++) {
-            printf("%d",arr[p]);
-        }
+//        SelectedSort(arr,7);
+//        for (int p = 0; p < 7; p++) {
+//            printf("%d",arr[p]);
+//        }
+        
+        //abvveaaerf
+        LongestPalindromicSubstring(@[@"a",@"b",@"v",@"c",@"c",@"v",@"b"]);
     }
     return 0;
 }
-
-//选择
-
-//插入
-
-//希尔
 
 
